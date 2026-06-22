d631395c70f7ab6a24ede79e03d3c0addb3b2839
Julia Mertz <info@juliamertz.dev>
2026-06-22 14:22:05 +0200

    feat: package with nix

--- flake.nix
+++ flake.nix
@@ -3,34 +3,60 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     systems.url = "github:nix-systems/default";
     nix-filter.url = "github:numtide/nix-filter";
+    crane.url = "github:ipetkov/crane";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-
   outputs = {
     nixpkgs,
+    crane,
     rust-overlay,
     systems,
+    nix-filter,
     ...
   }: let
-    eachSystem = nixpkgs.lib.genAttrs (import systems);
     overlays = [(import rust-overlay)];
+    filter = import nix-filter;
+    eachSystem = nixpkgs.lib.genAttrs (import systems);
 
-    rustBinFor = pkgs: pkgs.rust-bin.nightly.latest;
+    toolchainFor = pkgs:
+      pkgs.rust-bin.nightly.latest.minimal.override {
+        extensions = ["clippy" "rust-src"];
+      };
   in {
+    packages = eachSystem (system: let
+      pkgs = import nixpkgs {inherit system overlays;};
+      craneLib = (crane.mkLib pkgs).overrideToolchain (toolchainFor pkgs);
+      src = filter {
+        root = ./.;
+        include = [
+          "src"
+          "Cargo.toml"
+          "Cargo.lock"
+        ];
+      };
+      craneArgs = {
+        inherit src;
+        strictDeps = true;
+      };
+      cargoArtifacts = craneLib.buildDepsOnly craneArgs;
+    in {
+      default = craneLib.buildPackage (craneArgs
+        // {
+          inherit cargoArtifacts;
+          pname = "brainpod-cli";
+          version = "0.1.0";
+          meta.mainProgram = "brainpod-cli";
+        });
+    });
+
     devShells = eachSystem (system: let
       pkgs = import nixpkgs {inherit system overlays;};
-      rust-bin = rustBinFor pkgs;
+      craneLib = (crane.mkLib pkgs).overrideToolchain (toolchainFor pkgs);
     in {
-      default = pkgs.mkShell {
-        nativeBuildInputs = with rust-bin; [
-          (minimal.override {
-            extensions = ["clippy" "rust-src"];
-          })
-        ];
-
+      default = craneLib.devShell {
         packages = with pkgs; [
           rustfmt
           rust-analyzer
